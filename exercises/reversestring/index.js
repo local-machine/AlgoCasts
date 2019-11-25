42e074256366d37bf879c9535005eb8694cc972a
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-11-24 18:41:38 -0800

    It will have multiple reverse string solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Long way .reverse()
+// function reverse(str) {
+//   const arr = str.split('');
+//   arr.reverse();
+//   return arr.join('');
+// }
 
 module.exports = reverse;
+
+// Short way .reverse()
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+//  Forof Loop
+// function reverse(str) {
+//   let reversed = '';
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+// Long way .reduce()
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '');
+// }
+
+// Short way .reduce()
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
